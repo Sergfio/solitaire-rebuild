c91e85b93fa3ec20daf02658b39670a251df3c79
StefanFocht <koechinnen.maechte0t@icloud.com>
2022-10-11 11:31:58 +0200

    Solitaire-Game-JS

---
+++ shell.script.js
@@ -0,0 +1,33 @@
+(function(){
+
+    const canvas = document.querySelector('canvas'),
+          ctx = canvas.getContext('2d');
+
+    let cardImage = new Image();
+    cardImage.src = './bmps/card_bmps/ace_hearts.png';
+    console.log(cardImage);
+
+    let AceHearts = {
+        img: cardImage,
+        x: 0,
+        y: 0
+    }
+
+    // let counter = 0;
+
+    function animate () {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        let { img, x, y } = AceHearts;
+
+        ctx.drawImage(img, x, y);
+        requestAnimationFrame(animate);
+
+        // counter += 2;
+        // ctx.drawImage(img, x += counter, y += counter);
+        // setTimeout(animate, 100)
+
+    }
+    animate();
+})()
